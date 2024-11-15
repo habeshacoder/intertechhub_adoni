9a2e343dd94aa528066216b1b1985e732f975d7e
AdoniasHaile <adoniashaile1@gmail.com>
2024-11-15 05:26:53 +0300

    add some modifications
    Added comments to clarify the purpose of each methods
    
    Removed unneccessary files and comments

--- stage_1/views.py
+++ stage_1/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse, HttpResponseNotFound
 
+# dictionary to return value for the incoming option path
+optionValues={"name":"Adonas Haile Mergeta","hobby":"Physical exercise, watching movies","dream":"First, land a job as a python developer, then create a start up"}
 
-optionValues={"name":"Adonas Haile Mergeta","hobby":"Physical exercise, watching movies","dream":"Landing a job with python developer then develope a start up"}
-
-
+# handles the incomeing request if there is no option values get passed from client
 def index(response):
    return HttpResponse('Welcome to InternTechHub backend interns stage_1 project')
-# This method returns an HttpResponse with the value of the option param.
+
+"""
+ handle the request if there is an option from the client.
+ if the option matches any of the optionValues keys, the method returns the corresponding value.
+
+"""
 def option(response,option):
     if option in optionValues:
       return HttpResponse(optionValues[option])
